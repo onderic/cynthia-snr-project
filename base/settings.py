d3e8409af6845523ee4172cf1d658036588dfe4e
onderic <ondericollins80@gmail.com>
2024-07-25 10:06:41 +0300

    login layout

--- base/settings.py
+++ base/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'plxdy4nevk=ev@mz=e!^dj5)x=i0uim009e=er6dqz$b_lh^8k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
